f44eaeae6e23e46a98f0455a6d27dff1caa8d597
Tiruc <187616772+Tiruc7@users.noreply.github.com>
2026-02-15 20:18:12 +0100

    Chapter 3 Writing methods

--- VCSBS/Chapter 3/DailyRate/DailyRate/Program.cs
+++ VCSBS/Chapter 3/DailyRate/DailyRate/Program.cs
@@ -4,14 +4,36 @@ namespace DailyRate
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
             (new Program()).run();
         }
 
         void run()
         {
+            double DailyRate = readDouble("Enter your daily rate: ");
+            int noOfDays = readInt("Enter number of days: ");
+            writeFee(calculateFee(DailyRate, noOfDays));
+        }
 
+        private void writeFee(double v) => Console.WriteLine($"The consultant's fee is: {v * 1.1}");
+
+
+        private double calculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+
+
+        private int readInt(String v)
+        {
+            Console.Write(v);
+            String line = Console.ReadLine();
+            return int.Parse(line);
+        }
+
+        private double readDouble(String v)
+        {
+            Console.Write(v);
+            String line = Console.ReadLine();
+            return double.Parse(line);
         }
     }
 }
